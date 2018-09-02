09ed276f07fd90f71a75c7f5fa5969709041b1bd
pensadotech <armando@pensadotech.com>
2018-09-01 20:02:34 -0700

    Give 60 seconds for the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -141,7 +141,7 @@ $(function () {
     let game = {
         isGameStarted: false,
         isGameOver: false,
-        maxTimeInSec: 40,
+        maxTimeInSec: 60,
         maxQuestions: 5,
         currentQuestion: 0,
         correctCount: 0,
